2228bea26b4cdce87eabfcb9afadb3b50b918c91
Joseph Diza <josephm.diza@gmail.com>
2022-04-05 12:45:34 -0700

    Add unit test for read_file

--- test/test_file.c
+++ test/test_file.c
@@ -31,10 +31,22 @@ void file_size_should_be_under_4gib() {
     TEST_ASSERT_TRUE(result);
 }
 
+void read_file_should_not_be_null() {
+    /* Read file shouldn't be null on files 
+    that exist */
+    char* contents = read_file("LICENSE");
+    TEST_ASSERT_NOT_NULL(contents);
+    /* Since we allocated memory with malloc
+       we must free the pointer here
+    */
+    free(contents);
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(file_size_should_return_correct_size);
     RUN_TEST(file_exists_should_return_true);
     RUN_TEST(file_size_should_be_under_4gib);
+    RUN_TEST(read_file_should_not_be_null);
     return UNITY_END();
 }
