f1315cfe6fe87728031f4219f0ca3a69da399ae3
Joseph Diza <josephm.diza@gmail.com>
2022-04-10 11:53:31 -0700

    Include C++ header guards for C++ support

--- include/command.h
+++ include/command.h
@@ -2,9 +2,18 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+/* Support header include for C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
 char* exec(const char* command, off_t size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // COMMAND_H
--- include/file.h
+++ include/file.h
@@ -2,6 +2,11 @@
 #ifndef FILE_H
 #define FILE_H
 
+/* Support header include for C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* https://linux.die.net/man/3/lseek64 */
 #define _FILE_OFFSET_BITS 64
 
@@ -31,6 +36,10 @@ bool under_limit(const char* path, off_t size);
 char* read_file(const char* path);
 char* read_slice(const char* path, off_t beg, off_t end);
 void write_file(const char* path, const char* contents);
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif // FILE_H
